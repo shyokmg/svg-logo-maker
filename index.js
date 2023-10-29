9be598189b4730440a707b2e668ae57b5f4690e4
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-10-29 12:11:33 +1100

    Added svg create file functionality and test scripts

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-// const gen = require('./lib/');
+const CLI = require('./lib/cli.js');
 
-const userInput = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter text upto 3 characters.'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a text color keyword (OR a hexadecimal number)',
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape from this list.',
-        choices: ['circle', 'triangle', 'square'],
-        loop: false
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter a shape color keyword (OR a hexadecimal number)',
-    },
-];
+const cli = new CLI();
 
-inquirer.prompt(userInput)
-    .then((data) => {
-        console.log('Successfull created SVG file')
-        // Create svg file
-    })
+cli.promptUser();
